c5874f28ed355963b15999a097a5bc71e75e6522
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-10-21 23:40:06 -0400

    added questions and required generateMarkdown.js local file

--- index.js
+++ index.js
@@ -1,44 +1,76 @@
-// const inquirer = require('inquirer');
-// const fs = require('fs');
-
 // inquirer
-//     .prompt([
-//         {
-//             type: 'input',
-//             message: 'What is your user name?',
-//             name: 'name',
-//         },
-//         {
-//             type: 'checkbox',
-//             message: 'What languages do you know',
-//             choices: ["HTML", "CSS", "JS"],
-//             name: 'languages',
-//         },
-//         {
-//             type: 'list',
-//             message: 'What is your preferred method of communication?',
-//             choices: ["LinkedIn", "GitHub", "Twitter"],
-//             name: 'communication',
-//         },
-//     ])
+//     .prompt(questions)
 //     .then((response) =>
 //         fs.appendFile('info.json', JSON.stringify(response, null, '\t'), (err) =>
 //             err ? console.error(err) : console.log('Commit logged!')
 //         )
 //     );
 
+// REQUIREMENTS TO PRINT
+// title, description, table of contents, 
+// installation instructions, usage, license, contributing, tests, and 
+// questions [github username, email]
+
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
 const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your PROJECT TITLE?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please DESCRIBE your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the INSTALLATION INSTRUCTIONS for your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What are the USAGE INFORMATION for your project?',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'Which LICENSE would you like?',
+        choices: ["MIT", "Apache", "BSD", "Unlicense"],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What are the CONTRIBUTION GUIDELINES for your project?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'What are the TEST INSTRUCTIONS for your project?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'What is your GITHUB USERNAME?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your EMAIL?',
+        name: 'email',
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) { }
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() { }
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
