8f0a63af3998462c6c0c581fe1f3386e88e2cb4e
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-10-21 23:02:37 -0400

    added inquirer to js

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+
+// inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             message: 'What is your user name?',
+//             name: 'name',
+//         },
+//         {
+//             type: 'checkbox',
+//             message: 'What languages do you know',
+//             choices: ["HTML", "CSS", "JS"],
+//             name: 'languages',
+//         },
+//         {
+//             type: 'list',
+//             message: 'What is your preferred method of communication?',
+//             choices: ["LinkedIn", "GitHub", "Twitter"],
+//             name: 'communication',
+//         },
+//     ])
+//     .then((response) =>
+//         fs.appendFile('info.json', JSON.stringify(response, null, '\t'), (err) =>
+//             err ? console.error(err) : console.log('Commit logged!')
+//         )
+//     );
+
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
